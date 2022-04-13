3afe9a63865b3143f0b0ef66edd483ab08ecd192
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2022-04-13 19:50:09 +0200

    Blinky STM32F100

--- includes/tools.h
+++ includes/tools.h
@@ -1,10 +1,13 @@
 #ifndef __TOOLS_H__
 #define __TOOLS_H__
 
-typedef enum
-{
-    SUCCESS = 0,
-    ERROR = 1
-} retcode_t
+#define LD4_Pin GPIO_PIN_8
+#define LD4_GPIO_Port GPIOC
+#define LD3_Pin GPIO_PIN_9
+#define LD3_GPIO_Port GPIOC
+#define TMS_SWDIO_Pin GPIO_PIN_13
+#define TMS_SWDIO_GPIO_Port GPIOA
+#define TCK_SWCLK_Pin GPIO_PIN_14
+#define TCK_SWCLK_GPIO_Port GPIOA
 
 #endif
