76ade5f74a46242f9a281f283386505462cc1c00
RadoslawSajdak <sajdak.radoslaw@gmail.com>
2022-05-31 16:16:14 +0200

    Moved API callbacks to the different file

---
+++ includes/api_callbacks.h
@@ -0,0 +1,41 @@
+#ifndef __API_CALLBACKS_H__
+#define __API_CALLBACKS_H__
+
+#include "bmp280.h"
+#include "mq2.h"
+#include "sts3x_dis.h"
+
+#define MAX_COMMAND_PARAMETERS          5
+
+typedef enum{
+    DEFAULT_COMMANDS = 0,
+    BMP_COMMANDS = 1,
+    STS_COMMANDS = 2,
+    MQ_COMMANDS = 3
+}sensor_choice_t;
+
+extern sensor_choice_t                  g_chosen_sensor;
+extern uint32_t                         g_command_parameters[MAX_COMMAND_PARAMETERS];
+extern uint8_t                          g_command_param_counter;
+/**** Rapers definitions *****/
+hub_retcode_t api_send_response(const uint32_t value);
+void api_menu_set_bmp(void);
+void api_menu_set_sts(void);
+void api_menu_set_mq(void);
+void api_menu_set_default(void);
+void api_bmp_get_data(void);
+void api_sts_get_temp(void);
+void api_sts_set_repeatability_low(void);
+void api_sts_set_repeatability_medium(void);
+void api_sts_set_repeatability_high(void);
+void api_sts_set_heater_on(void);
+void api_sts_set_heater_off(void);
+void api_sts_soft_reset(void);
+void api_mq2_get_sample(void);
+void api_mq2_get_LPG(void);
+void api_mq2_get_CO(void);
+void api_mq2_get_SMOKE(void);
+void api_mq2_set_tracking(void);
+void api_mq2_clear_tracking(void);
+
+#endif
--- src/api.c
+++ src/api.c
@@ -5,11 +5,8 @@
 #include "debug_uart.h"
 #include "api_uart.h"
 #include "tools.h"
-#include "string.h"
 #include "timers.h"
-#include "bmp280.h"
-#include "sts3x_dis.h"
-#include "mq2.h"
+#include "api_callbacks.h"
 
 
 /***** Defines *****/
@@ -22,20 +19,11 @@ enum {
     MAX_COMMAND_NUMBER = 20,
     MESSAGE_BUFFER = 10 * MAX_MSG_LEN,
     WAIT_FOR_MSG_TIMEOUT_MS = 1000,
-    MAX_COMMAND_CHOICE = 4,
-    MAX_COMMAND_PARAMETERS = 5
+    MAX_COMMAND_CHOICE = 4
 };
 
 
 typedef void (*api_callback)(void);
-
-typedef enum{
-    DEFAULT_COMMANDS = 0,
-    BMP_COMMANDS = 1,
-    STS_COMMANDS = 2,
-    MQ_COMMANDS = 3
-}sensor_choice_t;
-
 
 
 typedef struct
@@ -45,52 +33,15 @@ typedef struct
 }command_handlers_s;
 
 /***** Local Functions Definitions *****/
-hub_retcode_t           api_send_response(const uint32_t value);
 hub_retcode_t           wait_for_message(char *message);
 hub_retcode_t           parse_at_command(void);
 static void             api_msg_timeout_handler(void);
 static uint32_t         str_to_u32(char *num_as_string);
 
-/**** Rapers definitions *****/
-static void api_menu_set_bmp(void);
-static void api_menu_set_sts(void);
-static void api_menu_set_mq(void);
-static void api_menu_set_default(void);
-static void api_bmp_get_data(void);
-static void api_sts_get_temp(void);
-static void api_sts_set_repeatability_low(void);
-static void api_sts_set_repeatability_medium(void);
-static void api_sts_set_repeatability_high(void);
-static void api_sts_set_heater_on(void);
-static void api_sts_set_heater_off(void);
-static void api_sts_soft_reset(void);
-static void api_mq2_get_sample(void);
-static void api_mq2_get_LPG(void);
-static void api_mq2_get_CO(void);
-static void api_mq2_get_SMOKE(void);
-static void api_mq2_set_tracking(void);
-static void api_mq2_clear_tracking(void);
-
-
-static void set_irq_pin(void)
-{
-    HAL_GPIO_WritePin(IRQ_GPIO, IRQ_PIN, 0);
-}
-static void clear_irq_pin(void)
-{
-    HAL_GPIO_WritePin(IRQ_GPIO, IRQ_PIN, 1);
-}
-
 
 /***** Local Variables *****/
 static bool                             g_msg_timeout = false;
 static char                             g_last_msg[MAX_MSG_LEN] = {0};
-static uint32_t                         g_command_parameters[MAX_COMMAND_PARAMETERS];
-static uint8_t                          g_command_param_counter = 0;
-static bmp280_data_s                    g_bmp_data = {0};
-static uint32_t                         g_sts32_val = 0;
-static measurement_config_t             g_sts32_config = REPEATABILITY_LOW;
-static sensor_choice_t                  g_chosen_sensor = DEFAULT_COMMANDS;
 static const command_handlers_s         __menu_command_list[MAX_COMMAND_NUMBER] = { {.command = "BMP", .callback = api_menu_set_bmp},
                                                                                     {.command = "STS", .callback = api_menu_set_sts},
                                                                                     {.command = "MQ", .callback = api_menu_set_mq},
@@ -137,12 +88,6 @@ void run_api(void)
 
 
 /***** Local Functions *****/
-hub_retcode_t api_send_response(const uint32_t value)
-{
-    char msg[20] = {0};
-    sprintf(msg, "%lu", value);
-    return api_uart_tx((uint8_t *)msg, strlen(msg));
-}
 
 void parse_parameters(char *msg)
 {
@@ -238,116 +183,4 @@ static uint32_t str_to_u32(char *num_as_string)
 static void api_msg_timeout_handler(void)
 {
     g_msg_timeout = true;
-}
-
-
-/***** Rapers *****/
-static void api_menu_set_bmp(void)
-{
-    g_chosen_sensor = BMP_COMMANDS;
-}
-static void api_menu_set_sts(void)
-{
-    g_chosen_sensor = STS_COMMANDS;
-}
-static void api_menu_set_mq(void)
-{
-    g_chosen_sensor = MQ_COMMANDS;
-}
-static void api_menu_set_default(void)
-{
-    g_chosen_sensor = DEFAULT_COMMANDS;
-}
-
-static void api_bmp_get_data(void)
-{
-    memset(&g_bmp_data, 0, sizeof(bmp280_data_s));
-    bmp280_get_data(&g_bmp_data);
-    api_send_response(g_bmp_data.temperature);
-}
-
-static void api_sts_get_temp(void)
-{
-    g_sts32_val = 0U;
-    sts3x_get_temperature(&g_sts32_val, g_sts32_config);
-    api_send_response(g_sts32_val);
-}
-
-static void api_sts_set_repeatability_low(void)
-{
-    g_sts32_config = REPEATABILITY_LOW;
-}
-
-static void api_sts_set_repeatability_medium(void)
-{
-    g_sts32_config = REPEATABILITY_MEDIUM;
-}
-
-static void api_sts_set_repeatability_high(void)
-{
-    g_sts32_config = REPEATABILITY_HIGH;    
-}
-
-static void api_sts_set_heater_on(void)
-{
-    sts3x_set_heater_status(STS3X_HEATER_ENABLE);
-}
-
-static void api_sts_set_heater_off(void)
-{
-    sts3x_set_heater_status(STS3X_HEATER_DISABLE);
-}
-
-static void api_sts_soft_reset(void)
-{
-    sts3x_soft_reset();
-}
-
-static void api_mq2_get_sample(void)
-{
-    uint16_t temp = mq2_get_sample();
-    clear_irq_pin();
-    api_send_response((uint32_t)temp);
-}
-
-static void api_mq2_get_LPG(void)
-{
-    uint16_t temp = mq2_get_ppm(LPG_MQ2);
-    clear_irq_pin();
-    api_send_response((uint32_t)temp);
-}
-
-static void api_mq2_get_CO(void)
-{
-    uint16_t temp = mq2_get_ppm(CO_MQ2);
-    clear_irq_pin();
-    api_send_response((uint32_t)temp);
-}
-
-static void api_mq2_get_SMOKE(void)
-{
-    uint16_t temp = mq2_get_ppm(SMOKE_MQ2);
-    clear_irq_pin();
-    api_send_response((uint32_t)temp);
-}
-
-static void api_mq2_set_tracking(void)
-{
-    if(g_command_param_counter != 3)
-    {
-        api_send_response(1);
-    }
-    else
-    {
-        mq2_track_data(g_command_parameters[0], (mq2_gas_t)g_command_parameters[1], (uint16_t)g_command_parameters[2], set_irq_pin);
-        api_send_response(0);
-    }
-
-    
-}
-
-static void api_mq2_clear_tracking(void)
-{
-    clear_irq_pin();
-    mq2_stop_tracking();
 }
